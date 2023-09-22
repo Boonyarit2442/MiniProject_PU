9a046229e02541d9188086ed1905c469ae4b1350
web4826 <6411130016@mut.ac.th>
2023-09-22 22:13:56 +0700

    test-web_1

---
+++ view/findjob.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>A113 Find Job</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+    <!-- Nav Start -->
+    <nav>
+      <div class="container">
+        <div class="nav-con">
+          <div class="logo">
+            <a href="#">Area<b>113</b></a>
+          </div>
+          <ul class="menu">
+            <li><a href="#" style="color: #f3a953">หางาน</a></li>
+            <li><a href="#">เข้าสู่ระบบ</a></li>
+            <li><a href="#">สมัครสมาชิก</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <hr />
+    <!-- Nav End -->
+
+    <!-- Banner Start -->
+    <section class="banner">
+      <div class="container">
+        <div class="banner-title">
+          <h1>งานทั้งหมด <a style="color: #f3a953">XX</a> อัตรา</h1>
+        </div>
+        <div class="banner-con">
+          <div class="banner-item">
+            <h2>Network</h2>
+            <hr />
+            <img src="icon/wifi-signal.png" />
+          </div>
+          <div class="banner-item">
+            <h2>Web Designer</h2>
+            <hr />
+            <img src="icon/web-design.png" />
+          </div>
+          <div class="banner-item">
+            <h2>IT Support</h2>
+            <hr />
+            <img src="icon/technical-support.png" />
+          </div>
+          <div class="banner-item">
+            <h2>Programmer & Developer</h2>
+            <hr />
+            <img src="icon/programmer.png" />
+          </div>
+          <div class="banner-item">
+            <h2>Database Management</h2>
+            <hr />
+            <img src="icon/database-management.png" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Banner End -->
+  </body>
+</html>
