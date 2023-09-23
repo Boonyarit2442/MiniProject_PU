228e2c9f4ffe4abb5b685d7b5e19cf41e9292ecf
jirawat-subpavithaya <6411130031@mut.ac.th>
2023-09-23 19:18:25 +0700

    php_Position_1

--- view/Position/position.php
+++ view/Position/position.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Position</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./posstyle.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body class="">
-    <?php require_once('../../layout/_layout.php')?>
+    <?php 
+    require_once('../../layout/_layout.php');
+    require_once("../../controler/postitionController.php");
+    ?>
     
     <section class="feature">
       <div class="feature-con">
@@ -51,76 +56,60 @@
           </tr>
         </thead>
         <tbody>
+          <?php for ($i=0; $i < count($Data); $i++) { ?>
           <tr>
             <th scope="row"><input type="checkbox" /></th>
-            <td>6411130001</td>
-            <td>Developer</td>
+            <td><?=$Data[$i]['ID_PST']?></td>
+            <td><?=$Data[$i]['NAME_PST']?></td>
             <td>
-              <a href="#"><img
+              <a data-toggle="modal" data-target="#myModal"><img
                 src="https://cdn-icons-png.flaticon.com/128/1828/1828270.png"
                 width="22"
-                height="22"
-              /></a>
+                height="22">
+              </a> 
               <a href="#"><img
                 src="https://cdn-icons-png.flaticon.com/128/2496/2496733.png"
                 width="22"
-                height="22"
-              /></a>
+                height="22">
+              </a>
             </td>
           </tr>
-          <tr>
-            <th scope="row"><input type="checkbox" /></th>
-            <td>6411130002</td>
-            <td>IT</td>
-            <td>
-            <a href="#"><img
-                src="https://cdn-icons-png.flaticon.com/128/1828/1828270.png"
-                width="22"
-                height="22"
-              /></a>
-              <a href="#"><img
-                src="https://cdn-icons-png.flaticon.com/128/2496/2496733.png"
-                width="22"
-                height="22"
-              /></a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row"><input type="checkbox" /></th>
-            <td>6411130003</td>
-            <td>HR</td>
-            <td>
-            <a href="#"><img
-                src="https://cdn-icons-png.flaticon.com/128/1828/1828270.png"
-                width="22"
-                height="22"
-              /></a>
-              <a href="#"><img
-                src="https://cdn-icons-png.flaticon.com/128/2496/2496733.png"
-                width="22"
-                height="22"
-              /></a>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row"><input type="checkbox" /></th>
-            <td>6411130004</td>
-            <td>Programer</td>
-            <td>
-            <a href="#"><img
-                src="https://cdn-icons-png.flaticon.com/128/1828/1828270.png"
-                width="22"
-                height="22"
-              /></a>
-              <a href="#"><img
-                src="https://cdn-icons-png.flaticon.com/128/2496/2496733.png"
-                width="22"
-                height="22"
-              /></a>
-            </td>
-          </tr>
+          <?php } ?>
         </tbody>
       </table>
+    </div>
+    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="CreateModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="CreateModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body w-100 h-100">
+                    <form action="../../controler/example_LoginControler.php" method="POST">
+                        <div class="">
+                            <!--from create data -->
+                            <div>
+                                <label for="NAME_ID">ID</label>
+                                <input type="text" name="NAME_ID" id="NAME_ID"><br>
+                            </div>
+                            <div>
+                                <label for="USER1">User</label>
+                                <input type="text" name="USER1" id="USER1"><br>
+                            </div>
+                            <div>
+                                <label for="PASSWORD">password</label>
+                                <input type="text" name="PASSWORD" id="PASSWOES"><br>
+                            </div>
+                            <div>
+                                <input type="submit" value="ADD" name="_method">
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
   </body>
 </html>
